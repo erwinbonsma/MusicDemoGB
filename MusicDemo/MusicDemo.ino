bdb6fed7fd37b21268b8af959ff14ebd43f1e4a8
Erwin Bonsma <eriban@users.sourceforge.net>
2020-04-27 21:43:30 +0200

    Show song info

--- MusicDemo/MusicDemo.ino
+++ MusicDemo/MusicDemo.ino
@@ -13,9 +13,24 @@ const Gamebuino_Meta::SongSpec* songs[4] = {
   zeptonSong2
 };
 
+const char *const titleBumbleBots = "Bumble Bots";
+const char *const titleRockForMetal = "Rock For Metal";
+const char *const titleZepton1 = "Zepton #1";
+const char *const titleZepton2 = "Zepton #2";
+const char *const creditsBumbleBots = "Paul Bonsma";
+const char *const creditsRockForMetal = "Jumalauta";
+const char *const creditsZepton = "rez";
+
 struct MetaSpec {
-  const char* title;
-  const char* credits;
+  const char *const title;
+  const char *const credits;
+};
+
+const MetaSpec songInfo[NUM_SONGS] = {
+  MetaSpec { .title =  titleBumbleBots, .credits = creditsBumbleBots },
+  MetaSpec { .title =  titleRockForMetal, .credits = creditsRockForMetal },
+  MetaSpec { .title =  titleZepton1, .credits = creditsZepton },
+  MetaSpec { .title =  titleZepton2, .credits = creditsZepton },
 };
 
 constexpr int HISTORY_LEN = 80;
@@ -23,16 +38,6 @@ int8_t cpuLoadHistory[HISTORY_LEN];
 int cpuLoadHistoryIndex;
 int8_t levelHistory[HISTORY_LEN];
 int levelHistoryIndex;
-
-const char* text =
-  "                     "
-  "Sound & Music demo     "
-  "Version 0.2     "
-  "Press A and B     "
-  "Coding by Erwin Bonsma     "
-  "Composition by Paul Bonsma";
-int textLen;
-int textIndex;
 
 void drawCpuHistory() {
   for (int x = HISTORY_LEN; --x >= 0; ) {
@@ -70,6 +75,9 @@ void update() {
     if (songIndex == NUM_SONGS) {
       songIndex = 0;
     }
+    if (gb.sound.isSongPlaying()) {
+      gb.sound.playSong(songs[songIndex], true);
+    }
   }
 
   cpuLoadHistory[cpuLoadHistoryIndex++] = gb.getCpuLoad();
@@ -80,33 +88,23 @@ void update() {
   if (levelHistoryIndex == HISTORY_LEN) {
     levelHistoryIndex = 0;
   }
-
-  textIndex++;
-  if ((textIndex >> 2) == textLen) {
-    textIndex = 0;
-  }
 }
 
 void draw() {
   gb.display.clear();
 
   gb.display.setColor(INDEX_WHITE);
-  gb.display.setCursor(4 - (textIndex % 4), 20);
-  gb.display.setTextWrap(false);
-  gb.display.print(text + (textIndex >> 2));
+  gb.display.setCursor(0, 0);
+  gb.display.println(songInfo[songIndex].title);
+  gb.display.printf("by %s\n", songInfo[songIndex].credits);
 
   drawCpuHistory();
   drawLevelHistory();
-
-  gb.display.setCursor(0, 0);
-  gb.display.print(gb.getFreeRam());
+  gb.display.println(gb.getFreeRam());
 }
 
 void setup() {
   gb.begin();
-  textLen = strlen(text);
-
-  zeptonSong1 = zeptonSong2;
 }
 
 void loop() {
